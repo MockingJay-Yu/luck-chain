9a1d24d95d6e52d0fd47bb043288a68becc0bbab
yudengke <yudengke1016@gmail.com>
2024-07-27 19:09:46 +0800

    test&deploy

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 - 用户可以通过购买一张彩票参与
 - 彩票的费用将在抽奖的时全部给赢家
 - 经过一段时间，彩票将自动选出一名赢家（使用Chainlink VRF，Chainlink Automation完成）
+
+forge install smartcontractkit/chainlink-brownie-contracts@0.6.1 --no-commit
---
+++ test/uint/RaffleTest.t.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
+import {Test, console} from "forge-std/Test.sol";
+
+import {Raffle} from "../../src/Raffle.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+
+contract RaffleTest is Test {
+    Raffle raffle;
+    HelperConfig helperConfig;
+
+    uint256 entranceFee;
+    uint256 interval;
+    address vrfCoordinator;
+    bytes32 gasLane;
+    uint64 subscriptionId;
+    uint32 callbackGasLimit;
+    address public PLAYER = makeAddr("player");
+    uint256 public constant STARTING_USER_BALANCE = 10 ether;
+
+    function setUp() external {
+        DeployRaffle deployer = new DeployRaffle();
+        (raffle, helperConfig) = deployer.run();
+        vm.deal(PLAYER, STARTING_USER_BALANCE);
+        (entranceFee, interval, vrfCoordinator, gasLane, subscriptionId, callbackGasLimit) =
+            helperConfig.activeNetWorkConfig();
+    }
+
+    function testRaffleInitializesInOpenState() public view {
+        assert(raffle.getRaffleState() == Raffle.RaffleState.OPEN);
+    }
+
+    function testRaffleRevertsWhenYouDontPayEnough() public {
+        vm.prank(PLAYER);
+        vm.expectRevert(Raffle.Raffle_NotEnoughETHSent.selector);
+        raffle.enterRaffle();
+    }
+
+    function testRaffleRecordsPlayerWhenTheyEnter() public {
+        vm.prank(PLAYER);
+
+        raffle.enterRaffle{value: entranceFee}();
+        address playerRecorded = raffle.getPlayer(0);
+        assert(playerRecorded == PLAYER);
+    }
+}
